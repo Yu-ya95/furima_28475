2815e7196f885e63a181e0bb4a57e172fca86d3e
Yu-ya95 <69030932+Yu-ya95@users.noreply.github.com>
2020-08-18 23:19:02 +0900

    出品ページのエラー文表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,8 +17,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    redirect_to "/"
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to "/"
+    else
+      render :new
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, :item_text, :price, :image, presence: true
+  validates :name, :item_text, :image, presence: true
 
-  validates :category_code, :status_code, :postage_code, :prefecture_code, :delivery_days_code, numericality: { other_than: 1 }
+  validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than => 10000000, message: 'Out of setting range, Half-width number'}
+
+  validates :category_code, :status_code, :postage_code, :prefecture_code, :delivery_days_code, numericality: { other_than: 1 , message: 'Select'}
 end
