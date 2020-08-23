69401f6f057bd06605cdfd8edf39c19dab446f7a
Yu-ya95 <69030932+Yu-ya95@users.noreply.github.com>
2020-08-23 17:24:08 +0900

    商品情報の更新機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,6 +15,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to action: :show
+  end
+
   def show
     @item = Item.find(params[:id])
   end
