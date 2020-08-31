42f8917794ec098a49e3eda3f3e9fc1009250756
Yu-ya95 <69030932+Yu-ya95@users.noreply.github.com>
2020-08-31 15:30:12 +0900

    不要な記述の削除

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -25,9 +25,9 @@ class PurchasesController < ApplicationController
 
   private
 
-  def order_params
-    params.permit(:token)
-  end
+  # def order_params
+  #   params.permit(:token)
+  # end
 
   def purchase_params
     params.require(:purchase_information).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number, :token).merge(user_id: current_user.id, item_id: @item.id, price: @item.price)
