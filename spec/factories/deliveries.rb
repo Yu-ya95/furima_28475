4113a5ee12017224e6c9bf9ac493fb670a2184e7
Yu-ya95 <69030932+Yu-ya95@users.noreply.github.com>
2020-08-31 15:30:48 +0900

    FactoryBotの記述

--- spec/factories/deliveries.rb
+++ spec/factories/deliveries.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :delivery do
-    
+    postcode { "123-4567" }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city { "市区町村" }
+    block { "番地" }
+    building { "建物名" }
+    phone_number { "09012345678" }
+    association :purchase
   end
 end
